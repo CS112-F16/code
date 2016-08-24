84fd58c5073229386eabfd4132c203d79c07e40d
Sami Rollins <srollins@cs.usfca.edu>
2016-08-24 10:42:36 -0700

    add skeleton for GradeCalculator

---
+++ ClassExamplesWeek1/GradeCalculator.java
@@ -0,0 +1,51 @@
+/*
+
+	A program that calculates a final letter grade based on 
+	scores for exams, homework, and projects.
+
+	Exams are worth 100 points and 30% of the final grade.
+	Homework is worth 100 points and is 10% of the final grade.
+	Projects are worth 200 points and are 60% of the final grade.
+
+	Final scores > 90% earn an A.
+	Final scores > 80% earn an B.
+	Final scores > 70% earn an C.
+	Final scores <= 70% earn an F.
+
+	@author Sami Rollins
+
+*/
+import java.util.Scanner;
+
+public class GradeCalculator {
+
+
+	public static void main(String[] args) {
+
+		//declare constants to hold distribution
+		final double EXAM_PERCENT = .3;
+		final double HW_PERCENT = .1;
+		final double PROJ_PERCENT = .6;
+
+		//declare constants to hold max points 
+		final int MAX_EXAM = 100;
+		final int MAX_HW = 100;
+		final int MAX_PROJECT = 200;
+
+		//declare constants to hold min values for letters
+		final int MIN_A = 90;
+		final int MIN_B = 80;
+		final int MIN_C = 70;
+
+		//set earned points		
+		
+
+		//calculate grade
+
+		//print result
+
+
+	}
+
+
+}
