41a4179734f9ea04ab41878ceaf72d7cf5130d35
Sami Rollins <srollins@cs.usfca.edu>
2016-09-27 16:37:19 -0700

    adding NumberList example

--- ClassExamplesWeek6/NumberList.java
+++ ClassExamplesWeek6/NumberList.java
@@ -6,11 +6,33 @@ public class NumberList {
 	
 	//data members
 	private int[] numbers;
+	private int count;
 
 	/**
 		Construct an empty list.
 	**/
 	public NumberList() {
+
+		numbers = new int[2];
+		count = 0;		
+
+	}
+
+	/**
+		Private method as it will not be invoked anywhere outside of
+		the class. 
+		Method will create a new array of twice the size and copy over
+		all elements of the existing array.
+	**/
+	private void resize() {
+
+		int[] newArray = new int[numbers.length*2];
+
+		for(int i = 0; i < numbers.length; i++) {
+			newArray[i] = numbers[i];
+		}
+		numbers = newArray;
+
 	}
 
 	/**
@@ -18,6 +40,17 @@ public class NumberList {
 		@param - newNumber
 	**/
 	public void addFirst(int newNumber) {
+
+		if(count >= numbers.length) {
+			resize();
+		}
+
+		for(int i = count; i > 0; i--) {
+			numbers[i] = numbers[i-1];
+		}
+
+		numbers[0] = newNumber;
+		count++;
 	}
 
 	/**
@@ -25,6 +58,14 @@ public class NumberList {
 		If the list is empty, do nothing.
 	**/
 	public void removeFirst() {
+
+		if(count > 0) {
+			for(int i = 0; i < count-1; i++) {
+				numbers[i] = numbers[i+1];
+			}
+
+			count--;
+		}
 	}
 
 
@@ -33,14 +74,35 @@ public class NumberList {
 		numbers.
 	**/
 	public boolean containsDuplicates() {
+
+		for(int i = 0; i < count-1; i++) {
+
+			for(int j = i+1; j < count; j++) {
+				if(numbers[i] == numbers[j]) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
  
 
 	/**
 		Return a string representation of the list in the format
 		{23, 4, 23, 66, 2}
+		23 4 23 66 2
 	**/
 	public String toString() {
+		String returnValue = "";
+
+
+		for(int i = 0; i < count; i++) {
+			returnValue += numbers[i] + " ";
+		}
+
+		return returnValue;
+
 	}
 
 }
--- ClassExamplesWeek6/NumberListDriver.java
+++ ClassExamplesWeek6/NumberListDriver.java
@@ -1,13 +1,36 @@
+//Test driven development
+
 public class NumberListDriver {
 
 	public static void main(String[] args) {
 
 		NumberList list = new NumberList();
 		
-		System.out.println(list);
+		System.out.println(list.toString()); //{}
 		
 		list.addFirst(2);		
-		System.out.println(list);		
+		System.out.println(list); //{2}
+
+		list.addFirst(7);		
+		System.out.println(list); //{7, 2}
+
+		list.addFirst(9);		
+		System.out.println(list); //{9, 7, 2}
+
+		list.addFirst(7);
+		System.out.println(list.containsDuplicates()); //true
+		list.removeFirst();
+
+		list.removeFirst();		
+		System.out.println(list); //{7 2}
+
+		list.removeFirst();		
+		System.out.println(list); //{2}
+
+		list.removeFirst();		
+		System.out.println(list); //{}
+
+		System.out.println(list.containsDuplicates()); //false
 
 	}
 
