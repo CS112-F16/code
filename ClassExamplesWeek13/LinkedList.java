3c0e31375155848291a7730a526119b5d6efbd63
Sami Rollins <srollins@cs.usfca.edu>
2016-11-28 10:15:48 -0800

    adding linkedlist example

--- ClassExamplesWeek13/LinkedList.java
+++ ClassExamplesWeek13/LinkedList.java
@@ -1,22 +1,132 @@
+/**
+	A class to implement a LinkedList data structure
+**/
 public class LinkedList {
 
 	private Node head;
+	private Node tail;
 
 	public LinkedList() {
 		head = null;
+		tail = null;
 	}
 
+	/**
+		Add a new node at the head (beginning) of the list.
+	**/
 	public void addAtHead(Integer data) {
+
 		Node newNode = new Node(data);
 		newNode.setNext(head);
 		head = newNode;
+
+		//if we are adding to an empty list
+		if(tail == null) {
+			//update tail
+			tail = newNode;
+		}
+
 	}
 
+	/**
+		Add a new node at the tail (end) of the list.
+	**/		
+	public void addAtTail(Integer data) {
+		Node newNode = new Node(data);
+
+		//special case: list is empty
+		if(head == null) {
+			head = tail = newNode;
+		} else {
+			tail.setNext(newNode);
+			tail = newNode;
+		}
+
+
+	}	
+
+	/**
+		Recursive implementation of a method to find a given item
+		in the list.
+	**/
+	// public boolean find(Integer data) {
+	// 	return find(data, head);
+	// }
+
+	// private boolean find(Integer data, Node current) {
+	// 	if(current == null) {
+	// 		return false;
+	// 	}
+	// 	if(current.getData().equals(data)) {
+	// 		return true;
+	// 	}
+	// 	return find(data, current.getNext());
+
+	// }
+
+	/**
+		Iterative implementation to find a given item in the list.
+	**/
+	public boolean find(Integer data) {
+		Node current = head;
+
+		while(current != null) {
+			if(data.equals(current.getData())) {
+				return true;
+			}
+			current = current.getNext();
+		}
+		return false;
+	}
+
+	/**
+		Return the data at the position specified by index.
+		Return -1 if index is out of range.
+	**/
+	public Integer get(int index) {
+		
+		Node current = head;
+
+		for(int i = 0; (i < index && current != null); i++) {
+			current = current.getNext();
+		}
+
+		if(current == null) {
+			return -1;
+		}
+		return current.getData();
+
+	}
+
+	/**
+		Delete the item at the head of the list.
+	**/
+	public void deleteHead() {
+
+		//delete from an empty list -- do nothing
+		if(head == null) {
+			return;
+		}
+
+		//advance head
+		head = head.getNext();
+
+		//make sure that we did not delete the last item
+		if(head == null) {
+			tail = null;
+		}
+
+	}
+
+	/**
+		Return a string representation of the list.
+	**/
 	public String toString() {
 
 		String result = "";
 
 		Node current = head;
+
 		while(current != null) {
 			result += current.getData() + " \n";
 			current = current.getNext();
--- ClassExamplesWeek13/LinkedListDriver.java
+++ ClassExamplesWeek13/LinkedListDriver.java
@@ -1,13 +1,32 @@
+/**
+	A class to test out the implementation of a LinkedList.
+**/
 public class LinkedListDriver {
 
 	public static void main(String[] args) {
 
 		LinkedList ll = new LinkedList();
-		ll.addAtHead(3);
+		// System.out.println("Empty List: \n" + ll.toString());
+		// System.out.println("find(3) - empty list : " + ll.find(3));
+
+
+		ll.addAtTail(3);
 		ll.addAtHead(12);
-		System.out.println(ll.toString());
+		ll.addAtHead(38);
 
+		// System.out.println(ll.toString());
+
+		System.out.println("get(0): " + ll.get(0));
+		System.out.println("get(1): " + ll.get(1));
+		System.out.println("get(2): " + ll.get(2));
+		System.out.println("get(-1): " + ll.get(-1));
+
+
+		// System.out.println("find(3): " + ll.find(3));
+		// System.out.println("find(12): " + ll.find(12));
+		// System.out.println("find(38): " + ll.find(38));
+		// System.out.println("find(322): " + ll.find(322));
+		// System.out.println("find(42): " + ll.find(42));
 	}
-
 
 }
